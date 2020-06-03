47880f5b4235a27fa782d9ee53640c8970d3fa74
Andrés Bravo <andresbravomontes89@gmail.com>
2020-06-03 20:17:58 +0200

    Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,13 +1,26 @@
 import React, { useState, useContext, useEffect } from "react";
 import AppContext from "../AppContext";
+import logo from "../logo-universidad-nebrija.jpg";
 
 import "./Styles.css";
 
 const Header = () => {
     return (
-        <div className="Header">
-            <h1>energy calculator</h1>
-        </div>
+        <div>
+            <div className="Header">
+                <div className="Logo">
+                    <img className="Image"
+                    src={logo} >
+                    </img>
+                </div>
+                    <div className="Text">
+                    <h1>Energy Calculator</h1>    
+                </div>    
+            </div>
+            <div className="Line">
+                <hr className="hr"></hr>
+            </div>
+        </div>  
     )
 }
 export {Header as default};
