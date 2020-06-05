08e5c61758adf94117b2ea17674cf40f4f3679ca
Andrés Bravo <andresbravomontes89@gmail.com>
2020-06-05 13:46:12 +0200

    Add energy by segments

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -135,23 +135,48 @@ const Query = {
       .find({ street: ObjectID(street) })
       .toArray();
 
+    let energy = 0; 
+    let total = 0;
     if (result) {
       let calculeGeneral = 0;
       let calculeSpecific = 0;
       const signals = result.map((obj) => obj);
       const filterSignal = result.filter((obj) => obj.signal != 0);
 
-      console.log(signals);
+      console.log("Jo: " + result.length);
 
-      const lengthGeneral = signals.length;
-      const lengthSpecific = filterSignal.length;
 
-      calculeGeneral = (lengthGeneral - lengthSpecific) * 2;
-      calculeSpecific = lengthSpecific * 1;
+      for (let i = 0; i<result.length; i+= 1) {
+          
+          if(i === 0 || i === result.length - 1){
+            energy = 33.72;
+            total = total + energy;
+            console.log("total: " + total+ i);
+          }else{
+            energy = 4.11;
+            total = total + energy;
+            console.log("total: " + total + i);
+          }
+         
+          
+        
+      }
+      console.log("total Final: " + total);
+      
+      // const lengthGeneral = signals.length;
+      // const lengthSpecific = filterSignal.length;
 
-      const r = calculeGeneral + calculeSpecific;
+      // calculeGeneral = (lengthGeneral - lengthSpecific) * 2;
+      // calculeSpecific = lengthSpecific * 1;
 
-      console.log(r);
+      // const numberSegmentsConstantValue = result.length - lengthSpecific;
+
+      // console.log("Por: " + numberSegmentsConstantValue);
+
+      // const r = calculeGeneral + calculeSpecific;
+
+      // console.log(lengthGeneral);
+      // console.log(r);
 
       return result;
     } else {
