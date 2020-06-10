3b62dd1fc1b3966afe506a4ecb5d5444cf8d63a4
Andrés Bravo <andresbravomontes89@gmail.com>
2020-06-10 19:47:09 +0200

    O2

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -247,33 +247,42 @@ const Query = {
       const A = resultValues.A;
       const G = resultValues.G;
       const fr = resultValues.fr;
+      const alfa = resultValues.alfa;
       let v = resultStreet.speed * (5 / 18);
       v = v.toFixed(2);
       let t = (0 - v) / -a;
       t = t.toFixed(1);
       const M = resultValues.M;
 
-      const auxEbreak1 = (M / 2) * (Math.pow(v - a * t, 2) - Math.pow(v, 2));
-      // const ebreak1 = auxEbreak1.toFixed(3);
-      
-      const auxEbreak2 =
+      const ebreak1 = (M / 2) * (Math.pow(v - a * t, 2) - Math.pow(v, 2));
+      const ebreak2 =
         (p * Cd * A * (Math.pow(v - a * t, 4) - Math.pow(v, 4))) / (-8 * a);
-      // const ebreak2 = auxEbreak2.toFixed(3);
-      
-      const auxEbreak3 = (M * G * fr) * (Math.pow(v - a * t, 2) - Math.pow(v, 2)) / (-2 * a);
-      // const ebreak3 = auxEbreak3.toFixed(3);
+      const ebreak3 = (M * G * fr) * (Math.pow(v - a * t, 2) - Math.pow(v, 2)) / (-2 * a);
 
-      let energyConsumed = auxEbreak1 + auxEbreak2 + auxEbreak3;
+      //Energy in braking
+      let energyBraking = ebreak1 + ebreak2 + ebreak3;
+      // energyBraking = energyBraking * 0.00027777777777778;
+      // energyBraking = energyBraking.toFixed(2);
 
-      console.log(auxEbreak1);
-      console.log(auxEbreak2);
-      console.log(auxEbreak3);
-      
-      
-      energyConsumed = energyConsumed * 0.00027777777777778
+      //Energy recovered in braking
+      let energyRecoveredInBraking = alfa * energyBraking;
+      energyRecoveredInBraking = energyRecoveredInBraking.toFixed(2);
+      //console.log(energyRecoveredInBraking);
+
+      const eacc1 = (M * (Math.pow(a * t, 2))) / 2;
+      const eacc2 = (p * Cd * A * (Math.pow(a * t, 4))) / (8 * a);
+      const eacc3 = (M * G * fr * (Math.pow(a * t, 2))) / (2 * a);
+
+      //Energy in accelration
+      let energyAcceleration = eacc1 + eacc2 + eacc3;
+      // energyAcceleration = energyAcceleration * 0.00027777777777778;
+      // energyAcceleration = energyAcceleration.toFixed(2);
+
+      let energyConsumed = energyAcceleration + alfa * energyBraking;
+      energyConsumed = energyConsumed * 0.00027777777777778;
       energyConsumed = energyConsumed.toFixed(2);
-      console.log(energyConsumed);
 
+      
       return energyConsumed;
     } else {
       return new Error("Insert correct ID");
