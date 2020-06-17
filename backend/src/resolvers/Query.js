7545d5df9b4d84ecad1aa8aeabe909129e602750
Andrés Bravo <andresbravomontes89@gmail.com>
2020-06-17 16:50:46 +0200

    subroute

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -211,7 +211,7 @@ const Query = {
       return new Error("Insert correct ID");
     }
   },
-  getO2: async (parent, args, ctx, info) => {
+  getO2Acceleration: async (parent, args, ctx, info) => {
     const { street, values } = args;
     const { client } = ctx;
 
@@ -269,6 +269,73 @@ const Query = {
       energyConsumed = energyConsumed.toFixed(2);
 
       return energyConsumed;
+    } else {
+      return new Error("Insert correct ID");
+    }
+  },
+  getO2Braking: async (parent, args, ctx, info) => {
+    const { street, values } = args;
+    const { client } = ctx;
+
+    const db = client.db("DataBase");
+    const collectionStreet = db.collection("Streets");
+    const collectionValues = db.collection("Values");
+
+    const resultStreet = await collectionStreet.findOne({
+      _id: ObjectID(street),
+    });
+    const resultValues = await collectionValues.findOne({
+      _id: ObjectID(values),
+    });
+
+    if (resultStreet && resultValues) {
+      const a = resultValues.a;
+      const p = resultValues.p;
+      const Cd = resultValues.Cd;
+      const A = resultValues.A;
+      const G = resultValues.G;
+      const fr = resultValues.fr;
+      const alfa = resultValues.alfa;
+      let v = resultStreet.speed * (5 / 18);
+      v = v.toFixed(2);
+      let t = (0 - v) / -a;
+      t = t.toFixed(1);
+      const M = resultValues.M;
+
+      const ebreak1 = (M / 2) * (Math.pow(v - a * t, 2) - Math.pow(v, 2));
+      const ebreak2 =
+        (p * Cd * A * (Math.pow(v - a * t, 4) - Math.pow(v, 4))) / (-8 * a);
+      const ebreak3 = (M * G * fr) * (Math.pow(v - a * t, 2) - Math.pow(v, 2)) / (-2 * a);
+
+      //Energy in braking
+      let energyBraking = ebreak1 + ebreak2 + ebreak3;
+      // energyBraking = energyBraking * 0.00027777777777778;
+      // energyBraking = energyBraking.toFixed(2);
+
+      //Energy recovered in braking
+      energyBraking = energyBraking * alfa;
+
+      energyBraking = energyBraking * 0.00027777777777778;
+
+
+      // energyRecoveredInBraking = energyRecoveredInBraking.toFixed(2);
+      //console.log(energyRecoveredInBraking);
+
+      // const eacc1 = (M * (Math.pow(a * t, 2))) / 2;
+      // const eacc2 = (p * Cd * A * (Math.pow(a * t, 4))) / (8 * a);
+      // const eacc3 = (M * G * fr * (Math.pow(a * t, 2))) / (2 * a);
+
+      // //Energy in accelration
+      // let energyAcceleration = eacc1 + eacc2 + eacc3;
+      // // energyAcceleration = energyAcceleration * 0.00027777777777778;
+      // // energyAcceleration = energyAcceleration.toFixed(2);
+
+      // let energyConsumed = energyAcceleration + alfa * energyBraking;
+      // energyConsumed = energyConsumed * 0.00027777777777778;
+      // energyConsumed = energyConsumed.toFixed(2);
+
+
+      return energyBraking;
     } else {
       return new Error("Insert correct ID");
     }
