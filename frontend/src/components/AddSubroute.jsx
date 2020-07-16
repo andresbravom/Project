0eb9a3807ab1362d11d85c12ec2d22a7455f4705
Andrés Bravo <andresbravomontes89@gmail.com>
2020-07-16 18:05:26 +0200

    add subroute

--- frontend/src/components/AddSubroute.jsx
+++ frontend/src/components/AddSubroute.jsx
@@ -4,11 +4,119 @@ import AppContext from "../AppContext";
 
 import "./Styles.css";
 
+const MUTATION = gql`
+  mutation AddSubroute(
+    $route: ID!
+    $name: String!
+    $lenght: Float!
+    $speed: Int!
+  ) {
+    addSubroute(route: $route, name: $name, lenght: $lenght, speed: $speed) {
+      _id
+      name
+      lenght
+      speed
+    }
+  }
+`;
+
 const AddSubroute = () => {
-    return (
-        <div>
-            Subroute
+  let inputRoute;
+  let inputName;
+  let inputLenght;
+  let inputSpeed;
+
+  const context = useContext(AppContext);
+
+  const [addSubroute, { data }] = useMutation(MUTATION);
+  const [click, setClick] = useState(false);
+
+  return (
+    <div className="AddSubroute">
+      <form
+        className="Module"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addSubroute({
+            variables: {
+              route: inputRoute.value,
+              name: inputName.value,
+              lenght: parseFloat(inputLenght.value),
+              speed: parseInt(inputSpeed.value),
+              
+            },
+          });
+          inputRoute.value = null;
+          inputName.value = null;
+          inputLenght.value = 0;
+          inputSpeed.value = 0;
+        }}
+      >
+        <div className="Fields">
+          <div className="Field">
+            <div className="Title">Insert Data</div>
+            <div>
+              <input
+                required
+                className="Input"
+                placeholder="Insert _id route"
+                ref={(node) => {
+                  inputRoute = node;
+                }}
+              />
+            </div>
+            <div>
+              <input
+                required
+                className="Input"
+                placeholder="Insert name Subroute"
+                ref={(node) => {
+                  inputName = node;
+                }}
+              />
+            </div>
+            <div>
+              <input
+                required
+                className="Input"
+                placeholder="Insert lenght Subroute"
+                ref={(node) => {
+                  inputLenght = node;
+                }}
+              />
+            </div>
+            <div>
+              <input
+                required
+                className="Input"
+                placeholder="Insert speed Subroute"
+                ref={(node) => {
+                  inputSpeed = node;
+                }}
+              />
+            </div>
+          </div>
         </div>
-    )
-}
+        {data ? (
+          data.addSubroute ? (
+            <div className="Status">Subroute created succesfully</div>
+          ) : click ? (
+            <div className="Status">Ops something went wrong</div>
+          ) : null
+        ) : null}
+        <button className="Botton" type="submit" onClick={() => setClick(true)}>
+          Add Subroute
+        </button>
+        <div
+          className="ButtonBar"
+          onClick={() => {
+            context.button.set(1);
+          }}
+        >
+          Back
+        </div>
+      </form>
+    </div>
+  );
+};
 export { AddSubroute as default };
