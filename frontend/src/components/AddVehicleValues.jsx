e19f8ed75adc3b8b817bfbddaa6025673d584cda
Andrés Bravo <andresbravomontes89@gmail.com>
2020-07-23 12:57:06 +0200

    addVehicleValues

--- frontend/src/components/AddVehicleValues.jsx
+++ frontend/src/components/AddVehicleValues.jsx
@@ -5,29 +5,11 @@ import AppContext from "../AppContext";
 import "./Styles.css";
 
 const MUTATION = gql`
-    mutation AddVehicleValues(
-        $p: Float!, 
-        $Cd: Float!, 
-        $A: Float!, 
-        $M: Float!, 
-        $G: Float!, 
-        $fr: Float!, 
-        $a: Float!, 
-        $alpha: Float!
-        ) {
-            addVehicleValues(
-                p: $p, 
-                Cd: $Cd, 
-                A: $A, 
-                M: $M, 
-                G: $G, 
-                fr: $fr, 
-                a: $a, 
-                alpha: $alpha,
-            ) {
-                _id
-            }
-        }
+  mutation AddVehicleValues($p: Float!, $Cd: Float!, $A: Float!, $M: Float!, $G: Float!, $fr: Float!, $a: Float!, $alpha: Float!) {
+    addVehicleValues(p: $p, Cd: $Cd, A: $A, M: $M, G: $G, fr: $fr, a: $a, alpha: $alpha) {
+      _id
+    }
+  }
 `;
 
 const AddVehicleValues = () => {
@@ -40,6 +22,54 @@ const AddVehicleValues = () => {
     let inputa;
     let inputAlpha;
 
-  return <div>Vehicle Values</div>;
+    const context = useContext(AppContext);
+
+    const [ addVehicleValues, {data} ] = useMutation(MUTATION);
+    const [ click, setClick ] = useState(false);
+
+  return (
+    <div className="AddRoute">
+      <form
+        className="Module"
+        onSubmit={(e) => {
+          e.preventDefault();
+          addRoute({ variables: { name: inputNameRoute.value } });
+          inputNameRoute.value = null;
+        }}
+      >
+        <div className="Fields">
+          <div className="Field">
+            <div className="Title">Insert Data</div>
+            <input
+              required
+              className="Input"
+              placeholder="Insert name route"
+              ref={(node) => {
+                inputNameRoute = node;
+              }}
+            />
+          </div>
+        </div>
+        {data ? (
+          data.addRoute ? (
+            <div className="Status">Route created succesfully</div>
+          ) : click ? (
+            <div className="Status">Ops something went wrong</div>
+          ) : null
+        ) : null}
+        <button className="Botton" type="submit" onClick={() => setClick(true)}>
+          Add Route
+        </button>
+        <div
+          className="ButtonBar"
+          onClick={() => {
+            context.button.set(1);
+          }}
+        >
+          Back
+        </div>
+      </form>
+    </div>
+  );
 };
 export { AddVehicleValues as default };
